e97a14a960df51b481c906fe60655a1a96ec3bfe
BtwToxic <btwalexxd@gmail.com>
2025-12-30 22:10:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = 
+API_ID = 21705136
+API_HASH = "78730e89d196e160b0f1992018c6cb19"
 
 bot = Client(
     "string_gen_bot",
