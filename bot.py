d1f4bd95ca25f9f597e0c527dfaa9a5ce0387133
BtwToxic <btwalexxd@gmail.com>
2026-01-02 21:10:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = "8524565104:AAFpLs0Qqj_MiogDVWwKm6VkQ67yNZZfo9I"
+BOT_TOKEN = "8524565104:AAGeD5CGThQxJCFQTyowPUgICJi-YuHJzhE"
 API_ID = 21705136
 API_HASH = "78730e89d196e160b0f1992018c6cb19"
 
